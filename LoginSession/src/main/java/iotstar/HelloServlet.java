574dd78476362b93af6b66143d01fd82fbe8e25c
Doan Quan Tuan <doanquantuan2005@gmail.com>
2025-08-22 20:18:21 +0700

    commit

---
+++ LoginSession/src/main/java/iotstar/HelloServlet.java
@@ -0,0 +1,45 @@
+package iotstar;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet(urlPatterns = { "/hello" })
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public HelloServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter printWriter = resp.getWriter();
+		HttpSession session = req.getSession();
+		if (session != null) {
+			String name = (String)session.getAttribute("username");
+			printWriter.println("LoginSession Xin chao " + name);
+		} else {
+			resp.sendRedirect("/HelloServlet/login");
+		}
+	}
+
+}
---
+++ LoginSession/src/main/java/iotstar/LoginServlet.java
@@ -0,0 +1,55 @@
+package iotstar;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet(urlPatterns= {"/login"})
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public LoginServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		req.getRequestDispatcher("/login.html").forward(req, resp);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String username = req.getParameter("username");
+		String password = req.getParameter("password");
+		
+		if (username.equals("tuan")&& password.equals("123")) {
+		
+			HttpSession session = req.getSession();
+			session.setAttribute("username", username);
+			resp.sendRedirect(req.getContextPath() + "/hello");
+		} 
+		else {
+			resp.sendRedirect("/LoginSession/login");
+		}
+	}
+
+}
